64ea83efd6619d55e08a558731dd1ad20a072cd1
alaaseddek <alaasedek315@gmail.com>
2023-07-07 00:31:33 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'macbook',
+            prices: 1400,
+            image: 'https://images.pexels.com/photos/303383/pexels-photo-303383.jpeg?cs=srgb&dl=pexels-veeterzy-303383.jpg&fm=jpg',
+
+        },
+        {
+            id: '2',
+            name: 'old car',
+            prices: 3000,
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp7A5RjDCq01UsFyzOiOo1glIF9u2uej8FN-qk63vK&s',
+
+        },
+        {
+            id: '3',
+            name: 'shoes',
+            prices: '1000',
+            image: 'https://media.istockphoto.com/id/1350560575/photo/pair-of-blue-running-sneakers-on-white-background-isolated.webp?b=1&s=170667a&w=0&k=20&c=liUSgX6SafJ7HWvefFqR9-pnf3KuH6v1lwQ6iEpePWc=',
+
+        },
+
+    ],
+};
+
+export default data;
